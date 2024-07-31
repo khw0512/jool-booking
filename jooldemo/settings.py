4c96c17f74eca6027bcd14fb42d983a28b1d7350
khw0512 <hwunderstand@gmail.com>
2024-07-31 20:55:05 +0900

    202407312055

--- jooldemo/settings.py
+++ jooldemo/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = "/sterna/data"
+LOGIN_REDIRECT_URL = "/data"
 LOGOUT_REDIRECT_URL = "/users/login"
 
 MEDIA_URL = "/media/"
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from users.forms import UserForm
 from reservation.models import Reservation
+
 # Create your views here.
 
 
@@ -11,15 +12,15 @@ def signup(request):
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             # stuffs = Stuff()
             # thisCart=Cart(user=user,stuffs=stuffs)
             # thisCart.save()
             # thisCart.user.add(request.user)
-            return render(request, 'sterna.html')
+            return render(request, "EN/sterna.html")
     else:
         form = UserForm()
-    return render(request, 'users/signup.html', {'form': form})
+    return render(request, "users/signup.html", {"form": form})
