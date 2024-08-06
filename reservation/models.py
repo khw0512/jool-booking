d909a18b54ab5d837a3524b3024aa51026871580
khw0512 <hwunderstand@gmail.com>
2024-08-06 15:15:17 +0900

    2408061515

--- reservation/models.py
+++ reservation/models.py
@@ -58,12 +58,16 @@ class Item(models.Model):
         (
             "bottom",
             (
+                ("XXS", "XXS"),
                 ("XS", "XS"),
                 ("S", "S"),
                 ("M", "M"),
                 ("L", "L"),
                 ("XL", "XL"),
                 ("XXL", "XXL"),
+                ("3XL", "3XL"),
+                ("4XL", "4XL"),
+                ("5XL", "5XL"),
             ),
         ),
     ]
